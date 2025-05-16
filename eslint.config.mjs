9bbf0c8f4132a35a56494afcc809ff24379c6457
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2025-05-16 12:20:16 +0900

    ⚙️ chore: 패키지 변경 및 lint 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { resolve } from "node:path";
 
 const project = resolve(process.cwd(), "tsconfig.json");
 
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
     "eslint:recommended",
