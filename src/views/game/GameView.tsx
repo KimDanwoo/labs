b3d46fcefaa2b1af3bb7731edec269f03a04bbf8
KimDanwoo <dudgh9198@naver.com>
2026-05-29 15:07:24 +0900

    feat: import 경로 수정

--- src/views/game/GameView.tsx
+++ src/views/game/GameView.tsx
@@ -3,31 +3,28 @@
 import { useAtomValue } from 'jotai';
 import { GAME_STATUS } from '@shared/constants';
 import { statusAtom } from '@entities/game/model/store';
-import {
-  useAutoDecay,
-  useCharacterMovement,
-} from '@entities/game/model/hooks';
+import { useAutoDecay, useCharacterMovement } from '@entities/game/model/hooks';
 import DeathScreen from '@views/death/DeathScreen';
 import { StatusBar } from '@widgets/status-bar/ui';
 import { Room } from '@widgets/game-room/ui';
 import { ActionButtons } from '@widgets/action-bar/ui';
-import { GameMessages, ModalRoot } from './ui';
+import { GameMessages, ModalRoot } from '@views/game/ui';
 
 export default function GameView() {
-  useAutoDecay();
-  useCharacterMovement();
+	useAutoDecay();
+	useCharacterMovement();
 
-  const status = useAtomValue(statusAtom);
+	const status = useAtomValue(statusAtom);
 
-  if (status === GAME_STATUS.DEAD) return <DeathScreen />;
+	if (status === GAME_STATUS.DEAD) return <DeathScreen />;
 
-  return (
-    <div className="flex flex-col flex-1 p-2 sm:p-3 gap-2 sm:gap-3">
-      <StatusBar />
-      <Room />
-      <ActionButtons />
-      <GameMessages />
-      <ModalRoot />
-    </div>
-  );
+	return (
+		<div className="flex flex-col flex-1 p-2 sm:p-3 gap-2 sm:gap-3">
+			<StatusBar />
+			<Room />
+			<ActionButtons />
+			<GameMessages />
+			<ModalRoot />
+		</div>
+	);
 }
